5f4a39621b5882975aa3b9b560ae54d99dc20524
lonebots <jishnusurajila@gmail.com>
2020-12-01 18:57:36 +0530

    component routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useParams } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/layout/Dashboard";
@@ -10,7 +10,8 @@ import EmployeRegister from "./components/employee/EmployeeRegister";
 import Footer from "./components/layout/Footer";
 import UserDashboard from './components/user/UserDashboard'
 import EmpDashboard from './components/employee/EmpDarshboard'
-import 
+import UpdateStock from './components/bloodbank/UpdateStock'
+import UpdateHealth from './components/bloodbank/UpdateHealth'
 import Donate from "./components/layout/Donate";
 
 import "./App.css";
@@ -20,12 +21,16 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
-     <Route exact path="/home" component={Dashboard} />
+        <Route exact path="/home" component={Dashboard} />
         <Route exact path="/donate" component={Donate} />
         <Route exact path="/login/usr" component={UserLogin} />
         <Route exact path="/login/emp" component={EmployeeLogin} />
         <Route exact path="/reg/usr" component={UserRegister} />
         <Route exact path="/reg/emp" component={EmployeRegister} />
+        <Route exact path="/login/usr/dash" component={UserDashboard} />
+        <Route exact path="/login/emp/dash" component={EmpDashboard} />
+        <Route exact path="/login/emp/ub" component={UpdateStock} />
+        <Route exact path="/login/emp/uh" component={UpdateHealth} />
       </Switch>
       <Footer />
     </div>
--- client/src/components/bloodbank/UpdateStock.js
+++ client/src/components/bloodbank/UpdateStock.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+const UpdateStock =()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default UpdateStock;
