951ce2e243e73f02ae48b57225532ddc3a96c621
lonebots <jishnusurajila@gmail.com>
2020-12-04 15:33:58 +0530

    user dashboard added

--- client/src/components/user/UserDashboard.js
+++ client/src/components/user/UserDashboard.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+//css
+import "../../assets/css/UserDash.css";
 
-const UserDashboard=()=>{
-    return(
-        <div>
+const UserDashboard = () => {
+  //donate function
+  const donate = () => {
+    alert("WE WILL GET IN TOUCH WITH YOU!");
+  };
 
-        </div>
-    )
-}
+  return (
+    <div className="user-dash">
+      <button onClick={donate}>DONATE</button>
+      <button>REQUEST</button>
+    </div>
+  );
+};
 export default UserDashboard;
